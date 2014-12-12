29e6c11a2787ef8777207045869c6c3f947d0135
Paul <galahadp@hotmail.com>
2014-12-11 18:13:35 -0800

    Revert "Revert "Add 'Help' and 'About' menus""
    This reverts commit e51b1c6020c1281d5e7f4f2c55829ad67885efd1.

--- source-code/src/cscd350/ayic/gui/MenuActionListener.java
+++ source-code/src/cscd350/ayic/gui/MenuActionListener.java
@@ -32,13 +32,31 @@ public class MenuActionListener implements ActionListener
 			_tracker.save();
 			break;
 		case "exit":
-			System.exit(0);
+			if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?",
+					"Exit", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
+				System.exit(0);
 			break;
 		case "about":
-
+			String about = "Pokemon Trivia Maze of DEATH\n\nv0.1\n\nBy team AYIC (And Yet It Compiles):\n";
+			about += "- David Stewart\n"
+					+ "- Samir Ouahhabi\n"
+					+ "- Paul Jones\n";
+			JOptionPane.showMessageDialog(null, about, "About", JOptionPane.OK_OPTION);
 			break;
 		case "help":
-
+			String help = "You are a forensic investigator that has been hired to solve a mysterious series of crimes.\n\n"
+					+ "You find yourself trapped in a maze, in order to solve the case you need to identify the dead Pokemon bodies before they rot.\n\n"
+					+ "You start in the northwest room and you work your way out going from one room to another until the end.\n\n"
+					+ "In order to successfully unlock a room you need to answer the presented question.\n"
+					+ "If you get the wrong answer, the room will be sealed forever.\n\n"
+					+ "The game ends when you reach the end room or when you get trapped and have no way to reach the end.\n\n"
+					+ "You are presented with a minimap that shows the whole 8x8 map in 3 different colors:\n"
+					+ "- Green: This room is unlocked, you can access it.\n"
+					+ "- Gray: This room is locked, you have to answer a question correctly to unlock it\n"
+					+ "- Red: This room is sealed forever, you can never access it again.\n\n"
+					+ "To navigate the map click on the arrows showing in the current room view.\n\n"
+					+ "Good luck in your quest!";
+			JOptionPane.showMessageDialog(null, help, "Help", JOptionPane.OK_OPTION);
 			break;
 		}
 	}
